6540c509ce7d0665443debd7b9f1ff22ccb29ad6
Jan Duplinszki <duplinszkijan@gmail.com>
2018-12-23 22:48:08 +0000

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,20 +4,18 @@ import { StyledLink } from '../navbar/navigation-styled-components'
 
 const Home = () => {
   return (
-    <div>
-      <Wrapper>
-        <Main>
-          <Card>
-            <Header>KONING</Header>
-            <SubHeader>-fightwear-</SubHeader>
-            <StyledLink to="/store">
-              <Button>OUR STORE</Button>
-            </StyledLink>
-            <SubHeader style={{marginTop: '100px', color: '#000'}}>Make a difference</SubHeader>
-          </Card>
-        </Main>
-      </Wrapper>
-    </div>
+    <Wrapper>
+      <Main>
+        <Card>
+          <Header>KONING</Header>
+          <SubHeader>-fightwear-</SubHeader>
+          <StyledLink to="/store">
+            <Button>OUR STORE</Button>
+          </StyledLink>
+          <SubHeader style={{marginTop: '100px', color: '#000'}}>Make a difference</SubHeader>
+        </Card>
+      </Main>
+    </Wrapper>
   )
 }
 
